c7ebb7511d090140b07587498bb0753904c391c8
crluser <crluser@posco.novalocal>
2017-04-17 11:41:28 +0800

    change host to 0.0.0.0

--- API/Sentiment/sentiment.py
+++ API/Sentiment/sentiment.py
@@ -31,4 +31,4 @@ def sentimentAnalysis():
     return jsonify({'sentiment':value}), 201
 
 if __name__ == '__main__':
-    app.run(port=5000,debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
