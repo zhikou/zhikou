48ced18fdfabeb0492cc03a90fcdc97f2f46e8b1
yuyyp@cn.ibm.com <yuyipeng2010@gmail.com>
2017-04-17 11:05:53 +0800

    API

---
+++ API/Sentiment/sentiment.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+#Sentiment API: http://localhost:5000/api/sentiment/v1
+
+from snownlp import SnowNLP
+from flask import Flask, jsonify
+from flask import abort
+from flask import make_response
+from flask import request
+from flask import url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World!\n"
+#curl -i -H "Content-Type: application/json" -X POST -d '{"utterance":"我很开心！"}' http://localhost:5000/api/sentiment/v1
+@app.route('/api/sentiment/v1', methods=['POST'])
+def sentimentAnalysis():
+    if not request.json:
+        abort(400)
+
+    text = request.json['utterance']
+    text_temp = text.replace('.','')
+    if(text_temp.isdigit()):
+    	value = 0.5
+    else:
+    	s = SnowNLP(text)
+    	value = round(s.sentiments,4)
+    return jsonify({'sentiment':value}), 201
+
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
