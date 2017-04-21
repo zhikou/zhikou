a8068543c7505aad1cd43b8562dab6eed157ab94
Bo Liu <wyyx_lb@163.com>
2017-04-22 00:01:28 +0800

    favicon

--- Dashboard/server/server.js
+++ Dashboard/server/server.js
@@ -2,25 +2,22 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
   // mongoose = require('mongoose'),
-  // bodyParser = require('body-parser'),
-  path = require('path'),
-  favicon = require('serve-favicon');
+  bodyParser = require('body-parser'),
+  path = require('path');
   
 // mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost/Tododb'); 
 
-// const public = path.join(__dirname,"../client/")
+const public = path.join(__dirname,"../client/")
 // console.log(public);
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use(favicon(path.join(__dirname,"../client/images/cool.ico")));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
  
 //set static resource
-// app.use(express.static(public))
+app.use(express.static(public))
 
-// var routes = require('./routes');
-// app.use("/", routes)
-app.use(favicon(path.join(__dirname,"../client/images/cool.ico")));
+var routes = require('./routes');
+app.use("/", routes)
 
 
 app.listen(port);
